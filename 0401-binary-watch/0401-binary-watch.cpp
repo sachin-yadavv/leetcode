60d062f941cdb04c3d746ed68f1df8d7f2e69583
Sachin Yadav <126893612+sachin-yadavv@users.noreply.github.com>
2026-02-17 23:42:12 +0530

    Time: 1 ms (25.79%), Space: 9.6 MB (65.38%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        // total set bits == no of pn led lights == turned on 
+        vector<string> result;
+
+        for (int HH = 0; HH <= 11; HH++) {
+
+           for (int MM = 0; MM <= 59; MM++) {
+
+                if (__builtin_popcount(HH) + __builtin_popcount (MM) == turnedOn) {
+                   string hour = to_string (HH);
+                   string minute = (MM < 10 ? "0" : "") + to_string (MM);
+
+                   result.push_back(hour + ":" + minute);
+                }
+
+           }
+        }
+
+          
+       return result;
+    }
+};
