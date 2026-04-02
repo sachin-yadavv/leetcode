6872a3898950af9c54906be82609367fd32041ed
Sachin Yadav <126893612+sachin-yadavv@users.noreply.github.com>
2026-04-02 14:02:39 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.51%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,53 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    stack<int>s1;
+    stack<int>s2;
+
+    
+    void push(int x) {
+        s1.push(x);
+        
+    }
+    
+    int pop() {
+        if(!s2.empty()) 
+        {int val = s2.top();
+            s2.pop();
+            return val;
+        }
+        else{
+             while(s1.size()){
+                s2.push(s1.top());
+
+                s1.pop();  
+             }
+
+        }
+         int val = s2.top();
+        s2.pop();
+        return val;
+        
+        
+    }
+    
+    int peek() {
+        if(!s2.empty()) return s2.top();
+        else{
+            while(s1.size()){
+                s2.push(s1.top());
+
+                s1.pop();  
+            }
+        }
+        return s2.top();
+        
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+        
+    }
+};
