fbfc89315546f0c0015284c2f9ae82c091536bd2
Sachin Yadav <126893612+sachin-yadavv@users.noreply.github.com>
2026-04-06 11:28:25 +0530

    Time: 0 ms (100%), Space: 27.1 MB (26.92%) - LeetHub

--- 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.cpp
+++ 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
-    long long safe_lcm(long long a, long long b) {
-        return (a / gcd(a, b)) * b;
+    long long maxScore(vector<int>& nums) {
+    int n = nums.size();
+
+    if(n == 1) return 1LL * nums[0] * nums[0];
+
+    vector<int> preG(n), sufG(n);
+    vector<long long> preL(n), sufL(n);
+
+    // prefix
+    preG[0] = nums[0];
+    preL[0] = nums[0];
+
+    for(int i = 1; i < n; i++) {
+        preG[i] = gcd(preG[i-1], nums[i]);
+        preL[i] = lcm(preL[i-1], (long long)nums[i]);
     }
 
-    long long maxScore(vector<int>& nums) {
-        int n = nums.size();
+    // suffix
+    sufG[n-1] = nums[n-1];
+    sufL[n-1] = nums[n-1];
 
-        if(n == 1) return 1LL * nums[0] * nums[0];
+    for(int i = n-2; i >= 0; i--) {
+        sufG[i] = gcd(sufG[i+1], nums[i]);
+        sufL[i] = lcm(sufL[i+1], (long long)nums[i]);
+    }
 
-        vector<int> preG(n), sufG(n);
-        vector<long long> preL(n), sufL(n);
+    long long ans = 1LL * preG[n-1] * preL[n-1];
 
-        // prefix
-        preG[0] = nums[0];
-        preL[0] = nums[0];
+    for(int i = 0; i < n; i++) {
+        int g;
+        long long l;
 
-        for(int i = 1; i < n; i++) {
-            preG[i] = gcd(preG[i-1], nums[i]);
-            preL[i] = safe_lcm(preL[i-1], nums[i]);
+        if(i == 0) {
+            g = sufG[i+1];
+            l = sufL[i+1];
         }
 
-        // suffix
-        sufG[n-1] = nums[n-1];
-        sufL[n-1] = nums[n-1];
-
-        for(int i = n-2; i >= 0; i--) {
-            sufG[i] = gcd(sufG[i+1], nums[i]);
-            sufL[i] = safe_lcm(sufL[i+1], nums[i]);
+        else if(i == n-1) {
+            g = preG[i-1];
+            l = preL[i-1];
         }
 
-        long long ans = 1LL * preG[n-1] * preL[n-1]; // 🔥 include full array
-
-        for(int i = 0; i < n; i++) {
-            int g;
-            long long l;
-
-            if(i == 0) {
-                g = sufG[1];
-                l = sufL[1];
-            }
-            else if(i == n-1) {
-                g = preG[n-2];
-                l = preL[n-2];
-            }
-            else {
-                g = gcd(preG[i-1], sufG[i+1]);
-                l = safe_lcm(preL[i-1], sufL[i+1]);
-            }
-
-            ans = max(ans, 1LL * g * l);
+        else {
+            g = gcd(preG[i-1], sufG[i+1]);
+            l = lcm(preL[i-1], sufL[i+1]);
         }
 
-        return ans;
+        ans = max(ans, 1LL * g * l);
+    }
+    return ans;
     }
 };
