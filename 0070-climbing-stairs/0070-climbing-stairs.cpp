faeff7efc5857748e5cbd1a4cc028216aac3be87
Sachin Yadav <126893612+sachin-yadavv@users.noreply.github.com>
2026-06-02 17:34:27 +0530

    Time: 0 ms (100%), Space: 8.8 MB (17.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        int a =1;
-        int b =2;
-        for(int i=3;i<=n;i++){
-            int c= a+b;
-            a=b;
-            b=c;
-        }
-        return b;
-        
+       vector<int>dp(n+1);
+       dp[0]=1;
+       dp[1]=1;
+       for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       } 
+       return dp[n];
     }
 };
