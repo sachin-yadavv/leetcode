510e3c9edd5bf3375f29bf7472c3ee4b63d19d79
Sachin Yadav <126893612+sachin-yadavv@users.noreply.github.com>
2026-06-22 16:35:54 +0530

    Time: 12 ms (38.88%), Space: 34.7 MB (99.59%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+
+        vector<pair<int,int>> adj[n];
+
+        for (auto &it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        const int MOD = 1e9 + 7;
+
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<pair<long long,int>>
+        > pq;
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if (dis > dist[node])
+                continue;
+
+            for (auto &it : adj[node]) {
+
+                int adjNode = it.first;
+                int edW = it.second;
+
+                if (dis + edW < dist[adjNode]) {
+
+                    dist[adjNode] = dis + edW;
+
+                    ways[adjNode] = ways[node];
+
+                    pq.push({dist[adjNode], adjNode});
+                }
+
+                else if (dis + edW == dist[adjNode]) {
+
+                    ways[adjNode] =
+                        (ways[adjNode] + ways[node]) % MOD;
+                }
+            }
+        }
+
+        return ways[n - 1];
+    }
+};
